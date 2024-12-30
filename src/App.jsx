391d147f312793d0836d6bdb11d5e0330945f5ef
Palakmaybe <palakverma2504@gmail.com>
2024-12-30 13:57:45 +0530

    two

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'; // Import `Route` along with `Routes`
-import Home from './pages/home.jsx';
+import Home from './pages/Home'; // Assuming Home component is in './pages/Home.jsx'
 
 import Doctors from './pages/Doctors';
 import Login from './pages/Login';
@@ -15,21 +15,20 @@ import Footer from './components/Footer';
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-       <Navbar/>
+       <Navbar />
        
       <Routes>
         <Route path='/' element={<Home />} /> 
         <Route path='/doctors' element={<Doctors />} />
         <Route path='/doctors/:speciality' element={<Doctors />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/my-profile' element={<MyProfile/>} />
-        <Route path='/my-appointments' element={<MyAppointment/>} />
-        <Route path='/appointment/:docId' element={<Appointment/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointment />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
-      <Footer/>
-      
+      <Footer />
     </div>
   );
 };
